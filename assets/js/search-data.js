e5e5c1b298d7044041a185f7ed3af40305a2b5ba
kamome1201 <kamome1201@users.noreply.github.com>
2025-04-18 05:42:23 +0000

    Deploying to gh-pages from @ kamome1201/kamome1201.github.io@1ba481b8dcd1aada16469024b1fd40c75d3fbb3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://kamome120.github.io"; 
-let updatedUrl = currentUrl.replace("https://kamome120.github.io", "");
+const siteUrl = "https://kamome1201.github.io"; 
+let updatedUrl = currentUrl.replace("https://kamome1201.github.io", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
